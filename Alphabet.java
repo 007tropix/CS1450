65e1f82380d0dc1611d9c0581fe21b98331a9169
007tropix <ryanduplooy015@gmail.com>
2021-12-09 12:50:02 -0700

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		char[] alphabet = {
+				'a', 'b', 'c', 'd',
+				'e', 'f', 'g', 'h',
+				'i', 'j', 'k', 'l',
+				'm', 'n', 'o', 'p',
+				'q', 'r', 's', 't',
+				'u', 'v', 'w', 'x',
+				'y', 'z'
+				};
+
+		for (int i = 0; i < alphabet.length; i++) {
+			System.out.println(alphabet[i]);
+		}
+	}
+
+}
