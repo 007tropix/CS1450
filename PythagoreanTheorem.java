1a36f4b66b480f8d0d7b8ddd40130f284493a6ec
007tropix <ryanduplooy015@gmail.com>
2022-02-01 09:04:31 -0700

    Update PythagoreanTheorem.java
    Added C O L O R

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -1,40 +1,45 @@
 import java.util.Scanner;
 
 public class PythagoreanTheorem {
-
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
 	public static void main(String[] args) {
+		
 		Scanner myScanner = new Scanner(System.in);
 		boolean pythLoop = true;
 
 		while (pythLoop) {
 			
-			System.out.println("Enter 0 for unknown value");
+			System.out.println(ANSI_CYAN + "Enter 0 for unknown value" + ANSI_RESET);
 			
-			System.out.println("\nInput value for a: ");
+			System.out.println(ANSI_CYAN + "\nInput value for a: " + ANSI_RESET);
 			double a = myScanner.nextDouble();
 
-			System.out.println("Input value for b: ");
+			System.out.println(ANSI_CYAN + "Input value for b: " + ANSI_RESET);
 			double b = myScanner.nextDouble();
 
-			System.out.println("Input value for c: ");
+			System.out.println(ANSI_CYAN + "Input value for c: " + ANSI_RESET);
 			double c = myScanner.nextDouble();
 
 			if (a == 0) {
 				a = Math.sqrt((c*c) - (b*b));
-				System.out.printf("\n%s %.3f", "a = ", a);
+				System.out.printf(ANSI_PURPLE + "\n%s %.3f", "a = " + ANSI_RESET, a);
 			}
 			
 			else if (b == 0) {
 				b = Math.sqrt((c*c) - (a*a));
-				System.out.printf("\n%s %.3f", "b = ", b);
+				System.out.printf(ANSI_PURPLE + "\n%s %.3f", "b = " + ANSI_RESET, b);
 			}
 			
 			else {
 				c = Math.sqrt((a*a) + (b*b));
-				System.out.printf("\n%s %.3f", "c = ", c);
+				System.out.printf(ANSI_PURPLE + "\n%s %.3f", "c = " + ANSI_RESET, c);
 			}
 
-			System.out.println("\nAgain? (y/n)");
+			System.out.println("\nAgain? (" + ANSI_GREEN + "y" + ANSI_RESET + "/" + ANSI_RED + "n" + ANSI_RESET + ")");
 			if (myScanner.next().equals("n")) {
 				pythLoop = false;
 
